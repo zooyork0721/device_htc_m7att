f41ba2a5550b8780a6a2a98614c4e7a28dccba26
Lars Greiss <kufikugel@googlemail.com>
2013-08-13 12:23:09 +0200

    Adjust slim.mk for new bootanimation
    Change-Id: I8878affaa6f7666c6f900d6b7fa05c44bf9f398e

--- slim.mk
+++ slim.mk
@@ -6,10 +6,6 @@ $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
-#bootanimation
-PRODUCT_COPY_FILES +=  \
-    vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
-
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7 BUILD_ID=JZO54K BUILD_FINGERPRINT="cingular_us/m7/m7:4.1.2/JZO54K/166754.12:user/release-keys" PRIVATE_BUILD_DESC="1.26.502.12 CL166754 release-keys"
 
 PRODUCT_NAME := slim_m7att
