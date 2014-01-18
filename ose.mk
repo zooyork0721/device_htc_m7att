72d33e30eb4dc6490c3509a15e71ebeaf9c85a3b
zooyork0721 <zooyork0721@gmail.com>
2014-01-18 00:04:56 -0500

    Change to OSE

--- ose.mk
+++ ose.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/htc/m7att/full_m7att.mk)
 
-$(call inherit-product, vendor/slim/config/gsm.mk)
+$(call inherit-product, vendor/ose/config/gsm.mk)
 
-$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/ose/config/nfc_enhanced.mk)
 
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+$(call inherit-product, vendor/ose/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7 BUILD_ID=JWR66V.H10 BUILD_FINGERPRINT="htc/m7_google/m7:4.3/JWR66V.H10/230993:user/release-keys" PRIVATE_BUILD_DESC="3.06.1700.10 CL230993 release-keys"
 
-PRODUCT_NAME := slim_m7att
+PRODUCT_NAME := ose_m7att
 PRODUCT_DEVICE := m7att
